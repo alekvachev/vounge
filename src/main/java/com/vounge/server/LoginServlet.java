b84a435de631a467d84445f3a6da6631b2041f6c
alekvachev <marmaladko@gmail.com>
2015-02-24 20:48:53 -0500

    Hard coded user authentication with cookie

--- src/main/java/com/vounge/server/LoginServlet.java
+++ src/main/java/com/vounge/server/LoginServlet.java
@@ -2,8 +2,8 @@ package com.vounge.server;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,44 +14,27 @@ import java.io.PrintWriter;
  * Created by Alek on 2/23/2015.
  */
 
-@WebServlet(
-        description = "Login Servlet",
-        urlPatterns = { "/LoginServlet" },
-        initParams = {
-                @WebInitParam(name = "user", value = "jamsterdam"),
-                @WebInitParam(name = "password", value = "edri4kii")
-        })
+@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-    public void init() throws ServletException {
-        //we can create DB connection resource here and set it to Servlet context
-        if(getServletContext().getInitParameter("dbURL").equals("jdbc:postgres://localhost/db") &&
-                getServletContext().getInitParameter("dbUser").equals("postgres") &&
-                getServletContext().getInitParameter("dbUserPwd").equals("postgres")) {
-            getServletContext().setAttribute("DB_Success", "True");
-        }
-        else throw new ServletException("DB Connection error");
-    }
+    private final String userID = "jamsterdam";
+    private final String password = "edri4kii";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         //get request parameters for userID and password
         String user = request.getParameter("user");
         String pwd = request.getParameter("pwd");
 
-        //get servlet config init params
-        String userID = getServletConfig().getInitParameter("user");
-        String password = getServletConfig().getInitParameter("password");
-        //logging example
-        log("User="+user+"::password="+pwd);
-
         if(userID.equals(user) && password.equals(pwd)) {
+            Cookie loginCookie = new Cookie("user", user);
+            //setting cookie to expiry in 30 mins
+            loginCookie.setMaxAge(30 * 60);
+            response.addCookie(loginCookie);
             response.sendRedirect("LoginSuccess.jsp");
         } else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out= response.getWriter();
             out.println("<font color=red>Either user name or password is wrong.</font>");
             rd.include(request, response);
-
         }
 
     }
--- src/main/webapp/LoginSuccess.jsp
+++ src/main/webapp/LoginSuccess.jsp
@@ -1,9 +1,7 @@
 <%--
-  Created by IntelliJ IDEA.
   User: Alek
   Date: 2/23/2015
   Time: 9:45 PM
-  To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
@@ -11,7 +9,26 @@
     <title>Login Success Page</title>
 </head>
 <body>
-    <h3>Hi user, login successful.</h3>
-    <a href="login.html">Login Page</a>
+<%
+    String userName = null;
+    Cookie[] cookies = request.getCookies();
+
+    if(cookies != null) {
+        for(Cookie cookie : cookies) {
+            if(cookie.getName().equals("user")) {
+                userName = cookie.getValue();
+                break;
+            }
+        }
+    }
+
+    if(userName == null) {
+        response.sendRedirect("login.html");
+    }
+%>
+    <h3>Hi <%= userName %>, Login successful.</h3><br>
+    <form action="LogoutServlet" method="post">
+        <input type="submit" value="Logout">
+    </form>
 </body>
 </html>
